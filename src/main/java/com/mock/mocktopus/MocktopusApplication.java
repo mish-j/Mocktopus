96802ca5aec99dd63ceacf056351003d51b58e69
mish-j <your-email@example.com>
2025-05-27 14:01:39 +0530

    Initial commit

---
+++ src/main/java/com/mock/mocktopus/MocktopusApplication.java
@@ -0,0 +1,13 @@
+package com.mock.mocktopus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocktopusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocktopusApplication.class, args);
+	}
+
+}
